21539aadf0f00ecea4aeea303d8ea754e32e4097
Sanchit Sahay <sanchitsahay.tech@gmail.com>
2021-05-23 23:39:22 +0530

    model training

---
+++ main.py
@@ -0,0 +1,13 @@
+import nltk
+import random
+from nltk.classify.scikitlearn import SklearnClassifier
+import pickle
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
+from nltk.classify import ClassifierI
+from statistics import mode
+from nltk.tokenize import word_tokenize
+import re
+
+trainfile = open('train_data.csv', 'r').read()
