011f8efcb242fe3647b6fe965be68eaf08073bb6
Fabhi <43674998+Fabhi@users.noreply.github.com>
2021-06-09 23:14:13 +0530

    Commited again

--- final/one_time/frontend/script.js
+++ final/one_time/frontend/script.js
@@ -1,82 +1,83 @@
 function fitElementToParent(el, padding) {
-    var timeout = null;
-    function resize() {
-      if (timeout) clearTimeout(timeout);
-      anime.set(el, {scale: 1});
-      var pad = padding || 0;
-      var parentEl = el.parentNode;
-      var elOffsetWidth = el.offsetWidth - pad;
-      var parentOffsetWidth = parentEl.offsetWidth;
-      var ratio = parentOffsetWidth / elOffsetWidth;
-      timeout = setTimeout(anime.set(el, {scale: ratio}), 10);
-    }
-    resize();
-    window.addEventListener('resize', resize);
+  var timeout = null;
+  function resize() {
+    if (timeout) clearTimeout(timeout);
+    anime.set(el, { scale: 1 });
+    var pad = padding || 0;
+    var parentEl = el.parentNode;
+    var elOffsetWidth = el.offsetWidth - pad;
+    var parentOffsetWidth = parentEl.offsetWidth;
+    var ratio = parentOffsetWidth / elOffsetWidth;
+    timeout = setTimeout(anime.set(el, { scale: ratio }), 10);
   }
-  
-  var advancedStaggeringAnimation = (function() {
-  
-    var staggerVisualizerEl = document.querySelector('.stagger-visualizer');
-    var dotsWrapperEl = staggerVisualizerEl.querySelector('.dots-wrapper');
-    var dotsFragment = document.createDocumentFragment();
-    var grid = [20, 10];
-    var cell = 55;
-    var numberOfElements = grid[0] * grid[1];
-    var animation;
-    var paused = true;
-    
-    fitElementToParent(staggerVisualizerEl, 0);
-    colors = ["red", "green", "orange", "pink", "blue", "yellow", "purple"]
-    for (var i = 0; i < numberOfElements; i++) {
-      var dotEl = document.createElement('div');
-      dotEl.classList.add('dot');
-      dotEl.setAttribute("id", i)
-      dotEl.addEventListener("click", function(){
-        
-        this.style.background= colors[Math.floor(Math.random() * colors.length)]
-        play(this.id);
-        
-      })
-      dotsFragment.appendChild(dotEl);
-    }
-    
-    dotsWrapperEl.appendChild(dotsFragment);
-  
-    // var index = anime.random(0, numberOfElements-1);
-    var index = 0;
-  
-    anime.set('.stagger-visualizer .cursor', {
-      translateX: anime.stagger(-cell, {grid: grid, from: index, axis: 'x'}),
-      translateY: anime.stagger(-cell, {grid: grid, from: index, axis: 'y'}),
-      translateZ: 0,
-      scale: 1.5,
-    });
-  
-    function play(index) {
-  
-      playing = true;
-      if (animation) animation.pause();
-  
-      // nextIndex = anime.random(0, numberOfElements-1);
-      animation = anime.timeline({
-        easing: 'easeInOutQuad',
-        // complete: play
-      })
+  resize();
+  window.addEventListener('resize', resize);
+}
+
+var advancedStaggeringAnimation = (function () {
+
+  var staggerVisualizerEl = document.querySelector('.stagger-visualizer');
+  var dotsWrapperEl = staggerVisualizerEl.querySelector('.dots-wrapper');
+  var dotsFragment = document.createDocumentFragment();
+  var grid = [20, 10];
+  var cell = 55;
+  var numberOfElements = grid[0] * grid[1];
+  var animation;
+  var paused = true;
+
+  fitElementToParent(staggerVisualizerEl, 0);
+  colors = ["red", "green", "orange", "pink", "blue", "yellow", "purple"]
+  for (var i = 0; i < numberOfElements; i++) {
+    var dotEl = document.createElement('div');
+    dotEl.classList.add('dot');
+    dotEl.setAttribute("id", i)
+    dotEl.addEventListener("click", function () {
+
+      this.style.background = colors[Math.floor(Math.random() * colors.length)]
+      document.querySelector("#_top").style.background = this.style.background;
+      play(this.id);
+
+    })
+    dotsFragment.appendChild(dotEl);
+  }
+
+  dotsWrapperEl.appendChild(dotsFragment);
+
+  // var index = anime.random(0, numberOfElements-1);
+  var index = 0;
+
+  anime.set('.stagger-visualizer .cursor', {
+    translateX: anime.stagger(-cell, { grid: grid, from: index, axis: 'x' }),
+    translateY: anime.stagger(-cell, { grid: grid, from: index, axis: 'y' }),
+    translateZ: 0,
+    scale: 1.5,
+  });
+
+  function play(index) {
+
+    playing = true;
+    if (animation) animation.pause();
+
+    // nextIndex = anime.random(0, numberOfElements-1);
+    animation = anime.timeline({
+      easing: 'easeInOutQuad',
+      // complete: play
+    })
       .add({
         targets: '.stagger-visualizer .cursor',
-        translateX: { value: anime.stagger(-cell, {grid: grid, from: index, axis: 'x'}) },
-        translateY: { value: anime.stagger(-cell, {grid: grid, from: index, axis: 'y'}) },
-        scale: 1.5, 
+        translateX: { value: anime.stagger(-cell, { grid: grid, from: index, axis: 'x' }) },
+        translateY: { value: anime.stagger(-cell, { grid: grid, from: index, axis: 'y' }) },
+        scale: 1.5,
         easing: 'cubicBezier(.075, .2, .165, 1)',
-        
-      }, "-=100" 
-         )
+
+      }, "-=100"
+      )
       .add({
         targets: '.stagger-visualizer .cursor',
         keyframes: [
-          { scale: .75, duration: 120}, 
-          { scale: 2.5, duration: 220},
-          { scale: 1.5, duration: 450},
+          { scale: .75, duration: 120 },
+          { scale: 2.5, duration: 220 },
+          { scale: 1.5, duration: 450 },
         ],
         duration: 300
       })
@@ -84,13 +85,13 @@ function fitElementToParent(el, padding) {
         targets: '.stagger-visualizer .dot',
         keyframes: [
           {
-            translateX: anime.stagger('-2px', {grid: grid, from: index, axis: 'x'}),
-            translateY: anime.stagger('-2px', {grid: grid, from: index, axis: 'y'}),
+            translateX: anime.stagger('-2px', { grid: grid, from: index, axis: 'x' }),
+            translateY: anime.stagger('-2px', { grid: grid, from: index, axis: 'y' }),
             duration: 100
           }, {
-            translateX: anime.stagger('4px', {grid: grid, from: index, axis: 'x'}),
-            translateY: anime.stagger('4px', {grid: grid, from: index, axis: 'y'}),
-            scale: anime.stagger([2.6, 1], {grid: grid, from: index}),
+            translateX: anime.stagger('4px', { grid: grid, from: index, axis: 'x' }),
+            translateY: anime.stagger('4px', { grid: grid, from: index, axis: 'y' }),
+            scale: anime.stagger([2.6, 1], { grid: grid, from: index }),
             duration: 225
           }, {
             translateX: 0,
@@ -99,11 +100,11 @@ function fitElementToParent(el, padding) {
             duration: 1200,
           }
         ],
-        delay: anime.stagger(80, {grid: grid, from: index})
+        delay: anime.stagger(80, { grid: grid, from: index })
       }, 30)
-      
-      // index = nextIndex;
-  
-    }
-  
-  })();
+
+    // index = nextIndex;
+
+  }
+
+})();
